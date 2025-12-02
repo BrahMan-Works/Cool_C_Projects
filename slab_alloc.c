11b68d69ba9ee27cdb5c9254ac709d26c0ae6387
BrahMan-Works <anonymous.gentleman020206@gmail.com>
2025-12-02 15:48:46 +0530

    Update slab_alloc.c

--- slab_alloc.c
+++ slab_alloc.c
@@ -15,6 +15,26 @@ typedef struct slab
     void* freeList;
 } slab_t;
 
+static slab_t* slabList[5] = {0};
+
+static int
+getClassIndex(size_t size)
+{
+    for(size_t i = 0; i < NUM_CLASSES; ++i)
+    {
+        if(size <= sizeClasses[i]) return i;
+    }
+    return -1;
+}
+
+static int
+ptrInSlab(slab_t* s, void* ptr)
+{
+    char* start = sizeof(slab_t) + (char*) s;
+    char* end = start + (s->capacity * s->objSize);
+    return (((char*)ptr >= start) && ((char*)ptr < end));
+}
+
 slab_t* slabCreate(size_t objSize)
 {
     slab_t* s = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
@@ -26,7 +46,6 @@ slab_t* slabCreate(size_t objSize)
     s->used = 0;
 
     char* objStart = (char*)s + sizeof(slab_t);
-
     char*p = objStart;
 
     for(size_t i = 0; i < s->capacity - 1; ++i)
@@ -42,25 +61,84 @@ slab_t* slabCreate(size_t objSize)
     return s;
 }
 
+void* slabAlloc(size_t size)
+{
+    int cI = getClassIndex(size);
+    if(cI == -1)
+    {
+        printf("unsupported size\n");
+        return NULL;
+    }
+
+    slab_t* s = slabList[cI];
+
+    while(s && s->freeList == NULL) s = s->next;
+
+    if(!s)
+    {
+        s = slabCreate(sizeClasses[cI]);
+        if(!s)
+        {
+            printf("slab creation falied\n");
+            return NULL;
+        }
+        s->next = slabList[cI];
+        slabList[cI] = s;
+    }
+
+    void* obj = s->freeList;
+    s->freeList = *(void**)obj;
+    s->used++;
+
+    return obj;
+}
+
+int slabFree(void* ptr, size_t size)
+{
+    if(!ptr) return -1;
+
+    int cI = getClassIndex(size);
+    if(cI == -1)
+    {
+        printf("invalid size\n");
+        return -1;
+    }
+
+    slab_t* s = slabList[cI];
+    while(s && !ptrInSlab(s, ptr)) s = s->next;
+
+    if(!s)
+    {
+        printf("invalid pointer\n");
+        return -1;
+    }
+
+    *(void**) ptr = s->freeList;
+    s->freeList = ptr;
+    s->used--;
+    
+    return 0;
+}
+
 int main()
 {
     /*
     code to check if this works as it should (it does).
+
+    void* a1 = slabAlloc(20);
+    void* a2 = slabAlloc(20);
+    void* a3 = slabAlloc(20);
+
+    printf("a1 = %p\n", a1);
+    printf("a2 = %p\n", a2);
+    printf("a3 = %p\n", a3);
+
+    slabFree(a2, 20);
+
+    void* a4 = slabAlloc(20);
     
-    for(size_t i = 0; i < NUM_CLASSES; ++i)
-    {
-        printf("slab for %zu bytes:\n", sizeClasses[i]);
-        slab_t* s = slabCreate(sizeClasses[i]);
-        if(s == NULL)
-        {
-            perror("slabCreate()");
-            return 1;
-        }
-
-        printf("slab: %p\ncapacity: %zu\n", s, s->capacity);
-        printf("free list: %p %p %p\n\n", s->freeList, s->freeList + (s->objSize), s->freeList + (2*s->objSize));
-    }
-
+    printf("a4 = %p\n", a4);
+    
     */
     return 0;
 }
